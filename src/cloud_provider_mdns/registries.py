e259c117115ea67e953bb07b157d950ef2c53aa4
Mathieu Imfeld <imfeldma@gmail.com>
2025-04-18 09:58:05 +0200

    Transfer

--- src/cloud_provider_mdns/registries.py
+++ src/cloud_provider_mdns/registries.py
@@ -1,9 +1,50 @@
 import typing
 import asyncio
 
-import kubernetes_asyncio as kubernetes
+from cloud_provider_mdns.base import Record, NSRecord, NSRecordUpdate
 
-from cloud_provider_mdns.base import NSRecord, NSRecordUpdate
+
+class Records:
+    def __init__(self):
+        self._records: typing.List[Record] = []
+        self._consumers: typing.Dict[str, asyncio.Queue] = {}
+
+    async def add_record(self, record: Record):
+        self._records.append(record)
+        self._records.add(record)
+        record.action = NSRecordUpdate.ADD
+        await self._notify_all(record)
+
+    async def modify_record(self, record: Record):
+        self._records.add(record)
+        record.action = NSRecordUpdate.MODIFY
+        await self._notify_all(record)
+
+    async def remove_record(self, record: Record):
+        self._records.remove(record)
+        record.action = NSRecordUpdate.REMOVE
+        await self._notify_all(record)
+
+    def subscribe(self, consumer: str) -> asyncio.Queue:
+        """
+        Subscribe to changes of the record registry
+        Args:
+            consumer (str): A unique identifier for the consumer
+
+        Returns:
+            A queue object to listen on for changes
+        """
+        self._consumers[consumer] = asyncio.Queue()
+        return self._consumers[consumer]
+
+    async def _notify_all(self, record: Record):
+        """
+        Notify all consumers of a change in the record registry
+        Args:
+            record (NSRecord): The record that has changed
+        """
+        for consumer in self._consumers.values():
+            await consumer.put(record)
 
 
 class RecordRegistry:
